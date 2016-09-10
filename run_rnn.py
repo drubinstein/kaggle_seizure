176547893e6a0e822e2ba0106dc1175ce03237b5
David Rubinstein <drubinstein91@gmail.com>
2016-09-10 18:39:38 -0400

    Actually making sense of RNN...them batch sizes

--- run_rnn.py
+++ run_rnn.py
@@ -18,14 +18,20 @@ import tensorflow as tf
 
 printing = False
 
+#Parameters
+learning_rate = .001
+batch_size = 128
+display_step = 10
+
 # Network Parameters
 n_channels = 16
 n_samps = 240000*n_channels
-samps_per_step = 240
-n_input = samps_per_step*n_channels # MNIST data input (img shape: 16 channels, 240 samples per channel)
+ch_samps_per_step = 240 #240 samples from one channel per step
+samps_per_step = ch_samps_per_step*n_channels
+n_input = samps_per_step
 n_steps = n_samps/n_input # timesteps
 n_hidden = 128 # hidden layer num of features
-n_classes = 1 # Total # classes (pre and postictal)
+n_classes = 2 # Total # classes (pre and postictal)
 
 
 def RNN(x, weights, biases):
@@ -42,7 +48,7 @@ def RNN(x, weights, biases):
     x = tf.split(0, n_steps, x)
 
     # Define a lstm cell with tensorflow
-    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0, state_is_tuple=True)
+    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
 
     # Get lstm cell output
     outputs, states = tf.nn.rnn(lstm_cell, x, dtype=tf.float32)
@@ -63,12 +69,9 @@ def main():
     """
 
     print('Initializing neural net')
-    #data_shape = data['dataStruct']['data'][0][0].shape
-    learning_rate = .001
-    dropout = .75
 
     # tf Graph input
-    x = tf.placeholder("float", [None, 1, n_input])
+    x = tf.placeholder("float", [None, n_steps, n_input])
     y = tf.placeholder("float", [None, n_classes])
 
     # Define weights
@@ -80,12 +83,16 @@ def main():
     }
 
     #create multilayer perceptron
-    pred = RNN(x,weights, biases)
+    pred = RNN(x, weights, biases)
 
     # Define loss and optimizer
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-    if printing: cost = tf.Print(cost,[cost],'MSE: ')
+    if printing: cost = tf.Print(cost,[cost],'Cost: ')
+
+    # Evaluate model
+    correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
     #open output file
     submission = open("submission.csv", "w")
@@ -102,6 +109,8 @@ def main():
 
             print "Training"
             folder = '{0}train_{1}/'.format(dataPath, dc)
+            num_files = len(os.listdir(folder))
+            fidx = 0
 
             for filename in os.listdir(folder):
                 if filename=='1_45_1.mat':
@@ -110,25 +119,53 @@ def main():
                 metadata = re.split(r'[_.]+',filename)
                 prepost = int(metadata[2]) # the class is the 3rd number
                 #now perform the batch processing
-                for step in xrange(0,n_steps):
-                    batch_x = data['dataStruct']['data'][0][0][(step*samps_per_step):((step+1)*samps_per_step)]
-                    batch_x = np.reshape(batch_x, (1,n_input))
-                    sess.run(optimizer, feed_dict={x: [batch_x], y: prepost})
+                #Reshape the data to get n_steps sequences of samps_per_step elements
 
-            print "Testing"
+                batch_x = np.empty([n_steps, samps_per_step])
+                batch_y = [1,0] if preopost == 0 else [0,1]
+                for s in xrange(n_steps):
+                    batch_x[s] = np.reshape( \
+                        data['dataStruct']['data'][0][0][ch_samps_per_step*s:ch_samps_per_step*(s+1)][:] \
+                        , (1,samps_per_step) \
+                        , order = 'F') #because this was originally matlab data....
+                sess.run(optimizer, feed_dict={x: [batch_x], y: [prepost]})
+
+                if fidx % display_step == 0:
+                    #Calculate batch accuracy
+                    acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
+                    #Calculate batch loss
+                    loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
+                    print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
+                                      "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                                                        "{:.5f}".format(acc))
+
+                fidx+=1
+                sys.stdout.write("\r{0}/{1}".format(fidx, num_files))
+                sys.stdout.flush()
+
+            print "\nTesting"
             folder = '{0}test_{1}/'.format(dataPath, dc)
+            num_files = len(os.listdir(folder))
+            fidx = 0
+
             for filename in os.listdir(folder):
                 data = sio.loadmat('{0}{1}'.format(folder, filename))
                 metadata = re.split(r'[_.]+',filename)
-                p = 0
-                for step in xrange(0,n_steps):
-                    batch_x = data['dataStruct']['data'][0][0][(step*samps_per_step):((step+1)*samps_per_step)]
-                    batch_x = np.reshape(batch_x, (1,n_input))
-                    p += sess.run(pred, feed_dict={x: [batch_x]})
+                batch_x = np.empty([n_steps, samps_per_step])
+                batch_y = [1,0] if preopost == 0 else [0,1]
+                for s in xrange(n_steps):
+                    batch_x[s] = np.reshape( \
+                        data['dataStruct']['data'][0][0][ch_samps_per_step*s:ch_samps_per_step*(s+1)][:] \
+                        , (1,samps_per_step) \
+                        , order = 'F') #because this was originally matlab data....
+                p = sess.run(pred, feed_dict={x: [batch_x]})
 
-                #now average p to figure out where it belongs
-                p = 0 if p/n_steps < .5 else 1
-                submission.write("{0},{1}\n".format(filename, p/n_steps))
+                submission.write("{0},{1}\n".format(filename, 0 if p[0] > p[1] else 1))
+                fidx+=1
+                sys.stdout.write("\r{0}/{1}".format(fidx, num_files))
+                sys.stdout.flush()
+
+            print ""
 
     submission.close()
 
